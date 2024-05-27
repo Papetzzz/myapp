dd5e5da96932f3c8869d0bc7cdda44acf98debd3
Papetzzz <delarocamarckjoseph16@gmail.com>
2024-05-27 16:13:04 +0800

    updatedCode

--- config/proxy.php
+++ config/proxy.php
@@ -1,6 +1,6 @@
 <?php
 // Forward the request to the NodeMCU
-$nodeMCU_response = file_get_contents("http://192.168.135.92/?pin=13");
+$nodeMCU_response = file_get_contents("http://192.168.178.92/?pin=13");
 
 // Echo the response back to the client
 echo $nodeMCU_response;
